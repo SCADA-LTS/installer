4a3638c0a59d4532031f64ae8eedf2a807cf3eba
grzesiekb <grzegorz.bylica@gmail.com>
2021-11-16 10:33:17 +0100

    add change context.xml

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,12 @@ use std::fs::File;
 use clap::{App, Arg};
 use rpassword::read_password;
 use std::io::Write;
-use quick_xml::Writer;
-use quick_xml::Reader;
-use quick_xml::events::{Event, BytesEnd, BytesStart};
-use std::iter;
+//use quick_xml::Writer;
+//use quick_xml::Reader;
+//use quick_xml::events::{Event, BytesEnd, BytesStart};
+use std::env;
+use std::fs;
+use std::path::Path;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 const DIR_TOMCAT_UNCONPRESED:&str = "apache-tomcat-9.0.48";
@@ -86,9 +88,108 @@ async fn fetch_and_uncopresed(to_fetch:Vec<Uncopresed>) -> Result<()>{
     Ok(())
 }
 
-async fn write_user_and_passwd_mysql(user:&str, passwd:&str) {
-    
+// async fn write_user_and_passwd_mysql(user:&str, passwd:&str) ->Result<()> {
+//     let filename = format!("./{}/conf/context.xml",DIR_TOMCAT_UNCONPRESED);
+//     println!("read file - {}", filename);
+//     let xml = fs::read_to_string(&filename)
+//     .expect("Something went wrong reading the file");
 
+//     let mut reader = Reader::from_str(&xml);
+//     reader.trim_text(true);
+//     let mut writer = Writer::new(Cursor::new(Vec::new()));
+//     let mut buf = Vec::new();
+//     loop {
+//         match reader.read_event(&mut buf) {
+//             Ok(Event::Start(ref e)) if e.name() == b"Resource" => {
+    
+//                 // crates a new element ... alternatively we could reuse `e` by calling
+//                 // `e.into_owned()`
+//                 let mut elem = BytesStart::owned(b"Resource".to_vec(), "Resource".len());
+    
+//                 // collect existing attributes
+//                 elem.extend_attributes(e.attributes().map(|attr| attr.unwrap()));
+    
+//                 // copy existing attributes, adds a new my-key="some value" attribute
+//                 elem.push_attribute(("username", user));
+//                 elem.push_attribute(("password", passwd));
+            
+        
+    
+//                 // writes the event to the writer
+//                 assert!(writer.write_event(Event::Start(elem)).is_ok());
+               
+//             },
+//             Ok(Event::End(ref e)) if e.name() == b"Resource" => {
+//                 assert!(writer.write_event(Event::End(BytesEnd::borrowed(b"Resource"))).is_ok());
+//             },
+//             Ok(Event::Eof) => break,
+//             Ok(e) => assert!(writer.write_event(e).is_ok()),
+//             // or using the buffer
+//             // Ok(e) => assert!(writer.write(&buf).is_ok()),
+//             Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+//         }
+//         buf.clear();
+        
+//     }
+    
+//     let result = writer.into_inner().into_inner();   
+//     let context = String::from_utf8_lossy(&result);
+//     println!("do zapisania {}", &context ); 
+//     fs::write(&filename, &context.to_string()).unwrap();
+
+//     // let result_xml = fs::read_to_string(&filename)
+//     // .expect("Something went wrong reading the file");
+
+//     // println!("xml:\n{}", result_xml);
+
+
+//     Ok(())
+
+// }
+                                                                  
+async fn create_config_xml(mysql_user:&str, mysql_passwd:&str) -> Result<()>{
+
+    let out_dir = format!("./{}/conf", DIR_TOMCAT_UNCONPRESED);
+    let dest_path = Path::new(&out_dir).join("context.xml");
+
+    let context = format!(
+        "
+        <Context>
+        <WatchedResource>WEB-INF/web.xml</WatchedResource>
+        <Resource 
+            name=\"jdbc/scadalts\" 
+            auth=\"Container\" 
+            type=\"javax.sql.DataSource\" 
+            factory=\"org.apache.tomcat.jdbc.pool.DataSourceFactory\" 
+            testWhileIdle=\"true\" 
+            testOnBorrow=\"true\" 
+            testOnReturn=\"false\" 
+            validationQuery=\"SELECT 1\" 
+            validationInterval=\"30000\" 
+            timeBetweenEvictionRunsMillis=\"30000\"
+            maxActive=\"80\" 
+            minIdle=\"10\" 
+            maxWait=\"10000\" 
+            initialSize=\"10\" 
+            removeAbandonedTimeout=\"1000\"
+            removeAbandoned=\"true\" 
+            abandonWhenPercentageFull=\"75\" 
+            logAbandoned=\"true\" 
+            minEvictableIdleTimeMillis=\"30000\" 
+            jmxEnabled=\"true\" 
+            jdbcInterceptors=\"org.apache.tomcat.jdbc.pool.interceptor.ConnectionState; org.apache.tomcat.jdbc.pool.interceptor.StatementFinalizer; org.apache.tomcat.jdbc.pool.interceptor.ResetAbandonedTimer; org.apache.tomcat.jdbc.pool.interceptor.SlowQueryReport(threshold=1500)\" 
+            username=\"{}\" 
+            password=\"{}\" 
+            driverClassName=\"com.mysql.jdbc.Driver\" 
+            defaultTransactionIsolation=\"READ_COMMITTED\" 
+            connectionProperties=\"useSSL=false\" 
+            url=\"jdbc:mysql://localhost:3306/scadalts\"/>
+        </Context>
+        ",mysql_user, mysql_passwd);
+
+    fs::write(&dest_path, context)?;
+
+    Ok(())
 }
 
 
@@ -121,13 +222,18 @@ async fn main() {
                             )
                           .get_matches();
 
+    //TODO welcome installer .. Scada-log_syntax!()
+    // remove no-extractr file
     println!("Start inst v{} for Scada-LTS v2.6.10", &env!("CARGO_PKG_VERSION"));
+
+    
 
     let mysql_user = matches.value_of("mysql_user").unwrap_or("root");
     let mysql_password = matches.value_of("mysql_password").unwrap_or("root");
     let ask_for_mysql_password = matches.index_of("ask_for_mysql_password");
     println!("MySql user: {}", mysql_user);
     println!("MySql password: {}", mysql_password);
+    //if ()
     println!("MySql ask for mysql password: {:?}", ask_for_mysql_password);
 
     if ask_for_mysql_password.is_some() {
@@ -142,8 +248,6 @@ async fn main() {
     //Embeded sql not now
     //let mysql_5_7_x86 = String::from("https://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-5.7.36-linux-glibc2.12-x86_64.tar.gz");
     //https://dev.mysql.com/doc/refman/5.7/en/windows-create-option-file.html
-
-    println!("Start inst v0.3.0 for Scada-LTS v2.6.10");
 
     //---
     let mut to_fetch_and_unpacking: Vec<Uncopresed> = Vec::new();
@@ -189,7 +293,7 @@ async fn main() {
     let mut to_fetch: Vec<Move> = Vec::new();
     
     let dir_webapps = format!("./{}/webapps", &DIR_TOMCAT_UNCONPRESED);
-    let dir_cfg = format!("./{}/conf", &DIR_TOMCAT_UNCONPRESED);
+    //let dir_cfg = format!("./{}/conf", &DIR_TOMCAT_UNCONPRESED);
 
     to_fetch.push(
     Move{
@@ -200,15 +304,15 @@ async fn main() {
         msg: String::from("Get Scada-LTS - and move to tomcat as ScadaBR.war"),
         to_dir: dir_webapps
     });
-    to_fetch.push(
-    Move{
-        featch: Featch{
-            url: String::from("https://github.com/SCADA-LTS/installer/releases/download/rv0.0.1/context.xml"),
-            file_name: String::from("context.xml")
-        },
-        msg: String::from("Get config context.xml - and move to tomcat"),
-        to_dir: dir_cfg
-    });    
+    // to_fetch.push(
+    // Move{
+    //     featch: Featch{
+    //         url: String::from("https://github.com/SCADA-LTS/installer/releases/download/rv0.0.1/context.xml"),
+    //         file_name: String::from("context.xml")
+    //     },
+    //     msg: String::from("Get config context.xml - and move to tomcat"),
+    //     to_dir: dir_cfg
+    // });    
     to_fetch.push(
     Move{
         featch: Featch{
@@ -256,11 +360,17 @@ async fn main() {
     });
 
     fetch_and_move(to_fetch).await.unwrap();
-    
+
+    //correct configuration
+    //write_user_and_passwd_mysql(mysql_user, mysql_password).await.unwrap();
+
+    //create configuration
+    create_config_xml(mysql_user, mysql_password).await.unwrap();
 
     //usuniecie dodakowych aplikacji menager itp
     //wylaczenie portu 8005
-    println!("If you have installed and running mysql server on localhost:3603 with user root and password root and the scadalts database is set up then you can run the \"./start.sh\" program");
+    println!("If you have installed and running mysql server on localhost:3603 with user {} and {} {} and the scadalts database is set up then you can run the \"./start.sh\" program",mysql_user,mysql_password,mysql_user);
+    println!("Start in webrowser - http://localhost:8080/ScadaBR");
 
     //---
     println!("end");
