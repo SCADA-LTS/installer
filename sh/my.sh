72aa3841de6511735590dfd95b73d142e3bb52c4
grzesiekb <grzegorz.bylica@gmail.com>
2022-03-14 06:19:04 +0100

    corrected mysql initial

--- sh/my.sh
+++ sh/my.sh
@@ -9,13 +9,22 @@ export TARGET="./mysql-5.7.36-linux-glibc2.12-x86_64"
 export DATADIR="../mysql/data"
 export PORT=9797
 export USER="root"
+export MySQL_INTERNAL_SCADA_LTS_LONG=`pwd`/mysql/log/logs.err
+export MY_LNG="./share/english"
 
-chmod -R 750 $TARGET
-chmod -R 750 ./mysql
+
+chmod -R 766 $TARGET
+chmod -R 766 ./mysql
+
+echo $MySQL_INTERNAL_SCADA_LTS_LONG
+echo $DATADIR
 
 cd $TARGET
 
 # start
 ./bin/mysqld --datadir $DATADIR \
  --bind-address=localhost \
- --port=$PORT 
+ --port=$PORT \
+ --language=$MY_LNG \
+ --log-error=$MySQL_INTERNAL_SCADA_LTS_LONG \
+ --lc-messages=en_US
--- sh/my_init.sh
+++ sh/my_init.sh
@@ -8,12 +8,15 @@ export TARGET="./mysql-5.7.36-linux-glibc2.12-x86_64"
 export DATADIR="../mysql/data"
 export MY_LNG="./share/english"
 export USER="root"
+export MySQL_INTERNAL_SCADA_LTS_LONG=`pwd`/mysql/log/logs.err
 
 
 rm -R ./mysql
 mkdir mysql 
 mkdir mysql/data
 
+echo $MySQL_INTERNAL_SCADA_LTS_LONG
+echo $DATADIR
 
 cd $TARGET
 
@@ -21,5 +24,7 @@ cd $TARGET
 # initialize
 ./bin/mysqld --datadir $DATADIR \
 --initialize-insecure --user=$USER \
---language=$MY_LNG
+--language=$MY_LNG \
+--log-error=$MySQL_INTERNAL_SCADA_LTS_LONG \
+--lc-messages=en_US
 
